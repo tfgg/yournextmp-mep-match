bf26e4471c93f98d9ca8549837b6a27c5d2fb38b
Tim Green <timothy.green@gmail.com>
2014-12-15 21:41:56 +0000

    reformat JSON to include eu2014 and ge2015 info separately

--- scripts/matcher.py
+++ scripts/matcher.py
@@ -2,19 +2,21 @@ import json
 import sys
 import requests
 
-endpoint1_url = sys.argv[1]
-endpoint2_url = sys.argv[2]
+from settings import yournextmp_url, yournextmep_url
 
-start_page = int(sys.argv[3])
+endpoint1_url = yournextmep_url
+endpoint2_url = yournextmp_url
 
-def get_instance_meta(instance_url):
-  return requests.get(instance_url).json()
+if len(sys.argv) > 1:
+  start_page = int(sys.argv[1])
+else:
+  start_page = 0
 
-instance1_meta = get_instance_meta(endpoint1_url)
-instance2_meta = get_instance_meta(endpoint2_url)
+def get_instance_meta(instance_url, meta_key):
+  return requests.get(instance_url).json()['meta'][meta_key]
 
-people_api_url1 = instance1_meta['meta']['persons_api_url']
-people_api_url2 = instance2_meta['meta']['persons_api_url']
+people_api_url1 = get_instance_meta(endpoint1_url, 'persons_api_url')
+people_api_url2 = get_instance_meta(endpoint2_url, 'persons_api_url')
 
 def get_page(start_page, people_api_url):
   page = requests.get(people_api_url + '?page={}&embed=membership.organization'.format(start_page)).json()
@@ -43,9 +45,7 @@ for i, people_page in enumerate(get_page(start_page, people_api_url1)):
   print >>sys.stderr, "Page", start_page + i
 
   for person in people_page['result']:
-    matches = list(find_persons_name(person['name']))
-
-    for person_match in matches:
+    for person_match in find_persons_name(person['name']):
       if person['id'] in confirmed_matches and person_match['id'] in confirmed_matches[person['id']]:
         print "Skipping one match for {}".format(person['name'])
         continue
@@ -57,9 +57,13 @@ for i, people_page in enumerate(get_page(start_page, people_api_url1)):
       standing_in_2015 = standing_in.get('2015', None)
 
       if standing_in_2015 is not None:
-        print "{} :: {}".format(person['name'], 'http://yournextmep.com/candidates/eu2014/' + person['identifiers'][0]['identifier'])
+        print "{} :: {}".format(person['name'],
+                                'http://yournextmep.com/candidates/eu2014/' + person['identifiers'][0]['identifier'])
 
-        print "  {} - {}, {}".format(person_match['name'], person_match['party_memberships']['2015']['name'], person_match['standing_in']['2015']['name']), 'http://yournextmp.com/person/{}'.format(person_match['id'])
+        print "  {} - {}, {}".format(person_match['name'],
+                                     person_match['party_memberships']['2015']['name'],
+                                     person_match['standing_in']['2015']['name']),
+                                     'http://yournextmp.com/person/{}'.format(person_match['id'])
 
         is_match = person['id'] in confirmed_matches and \
                    person_match['id'] in confirmed_matches[person['id']] and \
@@ -77,9 +81,12 @@ for i, people_page in enumerate(get_page(start_page, people_api_url1)):
         match = {'yournextmep_url': person['url'],
                  'yournextmp_url': person_match['url'],
                  'name': person['name'],
-                 'party_2015': person_match['party_memberships']['2015']['name'],
-                 'constituency_2015': person_match['standing_in']['2015']['name'],
-                 'match': is_match}
+                 'match': is_match,
+                 'eu2014': {'area': person['memberships'][0]['label'][len("European electoral candidate for "):],
+                            'party': person['memberships'][0]['organization_id']['name'],},
+                 'ge2015': {'party': person_match['party_memberships']['2015']['name'],
+                            'constituency': person_match['standing_in']['2015']['name'],},
+                }
 
         if person['id'] not in confirmed_matches:
           confirmed_matches[person['id']] = {}
